d24397a1cbe7eead884b74c349c849a57913dc04
Ilya Dubov <pikkvile@gmail.com>
2016-11-11 14:42:14 +0300

    deployement automation

--- bin/move.sh
+++ bin/move.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 scp ~/code/waste/target/waste.tar.gz root@oakenphoto.ru:/tmp
+ssh root@oakenphoto.ru "cd /opt/node/waste && tar zxf /tmp/waste.tar.gz && npm install && cd ./public && bower install --allow-root && forever restart /opt/node/waste/bin/www"
